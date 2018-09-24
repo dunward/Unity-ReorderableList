8de8fc02a6bec1a474831ace7d8a0a70991511bf
eglik <eglik@odin.vision>
2018-09-25 04:00:13 +0900

    feat: Sample ReorderableList Add

---
+++ ReorderableList/Assets/Scripts/SampleEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor(typeof(Sample))]
+public class SampleEditor : Editor
+{
+	private ReorderableList list;
+
+	private void OnEnable()
+	{
+		list = new ReorderableList(serializedObject, serializedObject.FindProperty("sampleData"), true, true, true, true);
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+		list.DoLayoutList();
+		serializedObject.ApplyModifiedProperties();
+	}
+}
